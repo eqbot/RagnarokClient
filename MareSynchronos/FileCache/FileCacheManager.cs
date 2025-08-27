16dfee1ab975e7596bd5454f87817ea96a65150a
eqbot <trentkenn8@gmail.com>
2025-08-27 14:54:15 -0700

    change expected hash length because Qm hashes aren't 40 in length

--- MareSynchronos/FileCache/FileCacheManager.cs
+++ MareSynchronos/FileCache/FileCacheManager.cs
@@ -508,7 +508,7 @@ public sealed class FileCacheManager : IHostedService
                 try
                 {
                     var hash = splittedEntry[0];
-                    if (hash.Length != 40) throw new InvalidOperationException("Expected Hash length of 40, received " + hash.Length);
+                    if (hash.Length != 46) throw new InvalidOperationException("Expected Hash length of 40, received " + hash.Length);
                     var path = splittedEntry[1];
                     var time = splittedEntry[2];
 
